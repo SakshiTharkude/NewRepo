528ef506b6c3ec59c3f497adfc60cf5d3540e313
SakshiTharkude <tharkudesakshi@gmail.com>
2022-12-20 15:19:33 +0530

    Revert "added method"
    This reverts commit 580d01500e06998a5d6c3a98dcd67c6f48ce1cb7.

--- src/main/java/demo1/Math.java
+++ src/main/java/demo1/Math.java
@@ -6,10 +6,6 @@ public class Math {
 		// TODO Auto-generated method stub
 		return a+b;
 	}
-	public static int add(int a, int b, int d) {
-		// TODO Auto-generated method stub
-		return a+b+d;
-	}
 	public static int multiple(int a, int b) {
 		// TODO Auto-generated method stub
 		return a*b;
